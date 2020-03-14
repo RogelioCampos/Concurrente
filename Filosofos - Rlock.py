9ae3c1341a0a1ef5b920342e679bc13ce126c650
Rogelio Campos <49373661+RogelioCampos@users.noreply.github.com>
2020-03-14 12:03:44 -0600

    Update Filosofos - Rlock.py
    Se agrega la condición de que muera el filosofo de inanición en caso de no poder comer.

--- Filosofos - Rlock.py
+++ Filosofos - Rlock.py
@@ -6,63 +6,73 @@ N = 5
 TIEMPO_TOTAL = 3
 
 class filosofo(threading.Thread):
-    semaforo = threading.Lock() #SEMAFORO BINARIO ASEGURA LA EXCLUSION MUTUA
-    estado = [] #PARA CONOCER EL ESTADO DE CADA FILOSOFO
-    tenedores = [] #ARRAY DE SEMAFOROS PARA SINCRONIZAR ENTRE FILOSOFOS, MUESTRA QUIEN ESTA EN COLA DEL TENEDOR
+    semaforo = threading.RLock()
+    estado = []
+    tenedores = []
     count=0
 
     def __init__(self):
-        super().__init__()      #HERENCIA
-        self.id=filosofo.count #DESIGNA EL ID AL FILOSOFO
-        filosofo.count+=1 #AGREGA UNO A LA CANT DE FILOSOFOS
-        filosofo.estado.append('PENSANDO') #EL FILOSOFO ENTRA A LA MESA EN ESTADO PENSANDO
-        filosofo.tenedores.append(threading.Semaphore(0)) #AGREGA EL SEMAFORO DE SU TENEDOR( TENEDOR A LA IZQUIERDA)
+        super().__init__()
+        self.id=filosofo.count
+        filosofo.count+=1
+        filosofo.estado.append('PENSANDO')
+        filosofo.tenedores.append(threading.Semaphore(0))
         print("FILOSOFO {0} - PENSANDO".format(self.id))
 
     def __del__(self):
-        print("FILOSOFO {0} - Se para de la mesa".format(self.id))  #NECESARIO PARA SABER CUANDO TERMINA EL THREAD       
+        print("FILOSOFO {0} - Se para de la mesa".format(self.id))
 
     def derecha(self,i):
-        return (i-1)%N #BUSCAMOS EL INDICE DE LA DERECHA
+        return (i-1)%N
 
     def izquierda(self,i):
-        return(i+1)%N #BUSCAMOS EL INDICE DE LA IZQUIERDA
+        return(i+1)%N
 
     def verificar(self,i):
         if filosofo.estado[i] == 'HAMBRIENTO' and filosofo.estado[self.izquierda(i)] != 'COMIENDO' and filosofo.estado[self.derecha(i)] != 'COMIENDO':
             filosofo.estado[i]='COMIENDO'
-            filosofo.tenedores[i].release()  #SI SUS VECINOS NO ESTAN COMIENDO AUMENTA EL SEMAFORO DEL TENEDOR Y CAMBIA SU ESTADO A COMIENDO
+            filosofo.tenedores[i].release()
 
     def run(self):
+        muerto = False
         for i in range(TIEMPO_TOTAL):
-            time.sleep(random.randint(0,5)) #CADA FILOSOFO SE TOMA DISTINTO TIEMPO PARA PENSAR, ALEATORIO
+            intentos = 0
+            time.sleep(random.randint(0,5))
             filosofo.estado[self.id] = 'HAMBRIENTO'
-            print("FILOSOFO {} HAMBRIENTO ".format(self.id))
-            while (not filosofo.semaforo.acquire()): #SEÑALA QUE TOMARA LOS TENEDORES (EXCLUSION MUTUA)
-                print("FILOSOFO {} VUELVE A PENSAR PERO CON HAMBRE ".format(self.id))
+            print("FILOSOFO {} HAMBRIENTO ".format(self.id)) 
+            while (not filosofo.semaforo.acquire()):
+                intentos += 1
+                print("FILOSOFO", self.id, "HA INTENTADO COMER", intentos, "VECES")
+                if intentos >= 10:
+                    muerto = True
+                    break
                 time.sleep(random.randint(0,2))
-            self.verificar(self.id) #VERIFICA SUS VECINOS, SI NO PUEDE COMER NO SE BLOQUEARA EN EL SIGUIENTE ACQUIRE
-            filosofo.semaforo.release() #SEÑALA QUE YA DEJO DE INTENTAR TOMAR LOS TENEDORES (CAMBIAR EL ARRAY ESTADO)
-            filosofo.tenedores[self.id].acquire() #SOLO SI PODIA TOMARLOS SE BLOQUEARA CON ESTADO COMIENDO
-            print("FILOSOFO {} COMIENDO ".format(self.id))
-            time.sleep(2) #TIEMPO ARBITRARIO PARA COMER
-            print("FILOSOFO {} TERMINO DE COMER ".format(self.id))
-            filosofo.semaforo.acquire() #SEÑALA QUE SOLTARA LOS TENEDORES
-            filosofo.estado[self.id] = 'PENSANDO'
-            self.verificar(self.izquierda(self.id))
-            self.verificar(self.derecha(self.id))
-            filosofo.semaforo.release() #YA TERMINO DE MANIPULAR TENEDORES
+            if muerto:
+                print("FILOSOFO", self.id, "HA MUERTO")
+                break
+            else:
+                self.verificar(self.id)
+                filosofo.semaforo.release()
+                filosofo.tenedores[self.id].acquire()
+                print("FILOSOFO {} COMIENDO ".format(self.id))
+                time.sleep(2)
+                print("FILOSOFO {} TERMINO DE COMER ".format(self.id))
+                filosofo.semaforo.acquire()
+                filosofo.estado[self.id] = 'PENSANDO'
+                self.verificar(self.izquierda(self.id))
+                self.verificar(self.derecha(self.id))
+                filosofo.semaforo.release()
 
 def main():
     lista=[]
     for i in range(N):
-        lista.append(filosofo()) #AGREGA UN FILOSOFO A LA LISTA
+        lista.append(filosofo()
 
     for f in lista:
-        f.start() #ES EQUIVALENTE A RUN()
+        f.start()
 
     for f in lista:
-        f.join() #BLOQUEA HASTA QUE TERMINA EL THREAD
+        f.join()
 
 if __name__=="__main__":
     main()
